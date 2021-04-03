ea4c65d186132f6c363075937a9fe5f75d8c1151
med100s <61676013+med100s@users.noreply.github.com>
2021-04-04 01:02:13 +0500

    Add files via upload

---
+++ reader.py
@@ -0,0 +1,29 @@
+from time import sleep
+import os
+
+f = open('voina-i-mir.txt', 'r', encoding='utf-8')
+
+
+f = str(list(f))
+b = r"!@#$n,'\—«–»:.-()"
+for char in b:
+    f = f.replace(char," ")
+f = f.split(' ')
+f = [x for x in f if x]
+
+#print(f)
+
+a = 0
+stop = 422
+for i in f:
+	if a == 100000:
+		break
+	if a < stop:
+		print(a, i)
+		a += 1
+	else:
+		print(a, i)
+		a += 1
+		sleep(0.1)
+		os.system('cls||clear')
+		
